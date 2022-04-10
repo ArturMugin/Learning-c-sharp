64e674c6e61d691fcea08db29ded646d35e5a955
ArturMugin <arturjancik@gmail.com>
2022-04-10 18:12:08 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using training;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program MyProgram = new Program();
+            MyProgram.doSometing();
+
+        }
+
+        public void doSometing()
+        {
+            User user = new User();
+            user.FirstName = "Arthur";
+            user.LastName = "Mugin";
+            //User.PrintUser(user);
+
+            User user2 = new User();
+            user2.FirstName = "Bob";
+
+
+            List<User> users = new List<User>();
+            users.Add(user);
+            users.Add(user2);
+
+            User.PrintUsers(users);
+
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace training
+{
+    internal class User
+    {
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}"; 
+            }
+
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Output(int times)
+        {
+            string message = "";
+            for (int i = 0; i < times; i++){ 
+                message += $"{FullName}\n";
+            }
+
+            return message;
+        }
+
+        public static void PrintUsers(List<User> users)
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.Output(1));
+            }
+        }
+
+        public static void PrintUser(User user)
+        {
+            Console.Write("Static method. Print user");
+            Console.WriteLine(user.Output(1));
+        }
+
+    }
+}
